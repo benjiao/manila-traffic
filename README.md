36e507c9a8b6cc40e2e056377c89721e5eefee3c
Benjie Jiao <benjiao12@gmail.com>
2015-09-15 05:17:44 +0800

    Added output file path as command line arguments

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Manila Traffic Monitor
 Fetches MMDA data and saves results into a JSON file.
+
+## To use:
+```python run.py /home/ubuntu/manila-traffic.json```
--- src/run.py
+++ src/run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import json
 import logging
 from manilatraffic import ManilaTraffic
@@ -17,20 +18,32 @@ if __name__ == '__main__':
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 
     # Create Handler (with own logging level)
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler('/var/log/manila-traffic.log')
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
     ch.setFormatter(formatter)
 
     # Add handler(s)
+    logger.addHandler(fh)
     logger.addHandler(ch)
 
     logger.info("Fetch traffic data...")
+
+    try:
+        filename = sys.argv[1]
+    except:
+        logger.info("Invalid arguments! To use: 'python run.py <path to outupt file>")
+        sys.exit(2)
 
     mt = ManilaTraffic(logger=logger)
     traffic_data = mt.fetch()
 
     if isinstance(traffic_data, dict):
-        with open(FILENAME, mode='a') as f:
+        with open(filename, mode='a') as f:
             f.write(json.dumps(traffic_data))
             f.write(os.linesep)
 
